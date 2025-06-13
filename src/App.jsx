5a41b41cf11fae9b312ff6031f6a9983d2b0dec2
dhifmohamed4 <dhifmohamed4@gmail.com>
2025-06-13 14:59:35 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import FeatureSection from "./components/FeatureSection";
-import Workflow from "./components/Workflow";
-import Footer from "./components/Footer";
-import Pricing from "./components/Pricing";
-import FAQ from "./components/faq";
-import Main from "./components/proj";
+import Main from "./pages/home";
 import LandingPage from "./pages/landing";
 
 const App = () => {
